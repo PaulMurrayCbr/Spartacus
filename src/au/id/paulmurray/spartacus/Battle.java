030cbd747b1cf0adc43d925e68983e54feb35752
Paul Murray <pmurray@bigpond.com>
2015-07-05 18:50:29 +1000

    More Spartacus!

---
+++ src/au/id/paulmurray/spartacus/Battle.java
@@ -0,0 +1,18 @@
+package au.id.paulmurray.spartacus;
+
+public class Battle {
+	public Battle(String[] av) {
+		// TODO Write Battle constructor
+		throw new UnsupportedOperationException();
+	}
+
+	public static void main(String[] av) throws Throwable {
+		new Battle(av).go();
+	}
+
+	private void go() {
+		// TODO Write go
+		throw new UnsupportedOperationException();
+	}
+
+}
